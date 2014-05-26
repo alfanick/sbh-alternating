651dd03648293909e16db362e9a40e1482328103
Amadeusz Juskowiak <amadeusz@amanointeractive.com>
2014-05-26 10:54:16 +0200

    added Reader stub

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include "reader.h"
+
 int main() {
   std::cout << "Hello World!\n";
 
---
+++ src/reader.h
@@ -0,0 +1,12 @@
+#ifndef PUT_SBH_ALT_READER_H_
+#define PUT_SBH_ALT_READER_H_
+
+namespace PUT {
+  namespace SBH {
+    class Reader {
+
+    };
+  };
+};
+
+#endif
