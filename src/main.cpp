2be03eaf92564a00577e658f2d1392a93e526516
Amadeusz Juskowiak <amadeusz@amanointeractive.com>
2014-05-27 06:34:08 +0200

    changed to object

--- src/main.cpp
+++ src/main.cpp
@@ -11,19 +11,14 @@ using namespace PUT::SBH;
 int main(int argc, char **argv) {
   std::string filepath = "data/example.seq";
 
-  if(argc > 1) 
+  if(argc > 1)
     filepath = argv[1];
 
-  PUT::SBH::Reader* r = new PUT::SBH::Reader(filepath);
+  Reader r(filepath);
 
-  std::cerr << "Odd: \n";
-  for (auto& l : r->sections[0]) {
-    std::cerr << l.first.sequence << " * " << l.second << std::endl;
-  }
+  std::cout << "Beginning from reader: " << r.beginning.sequence << "\n";
 
-  std::cout << "Beginning from reader: " << r->beginning.sequence << "\n";
-
-  Sequencer sequencer(r->sections, r->length, r->sample_length, r->beginning.sequence);
+  Sequencer sequencer(r.sections, r.length, r.sample_length, r.beginning.sequence);
   sequencer.run();
 
   return 0;
