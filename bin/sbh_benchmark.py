f55343e8d60d78fa357294d2555b79bc7a802f29
Amadeusz Juskowiak <amadeusz@amanointeractive.com>
2014-06-09 11:08:25 +0200

    testing api

--- bin/sbh_benchmark.py
+++ bin/sbh_benchmark.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 
-from psutil import Process
+# from psutil import Process
 from argparse import ArgumentParser
-import cPickle as pickle
+# import cPickle as pickle
+import seq_gen as sbh
 
 if __name__ == "__main__":
     parser = ArgumentParser()
 
     args = parser.parse_args()
+
+    print sbh.generate_spectrum(output=open("/dev/null", "a"), length=20)
